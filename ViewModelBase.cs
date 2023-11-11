434a7feeab077c8d646d5cdb1b12cf2e158937e9
Gigg <danya19981017@gmail.com>
2023-11-11 22:57:15 +0100

    Добавьте файлы проекта.

---
+++ ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _01._11._2023_Команды.MVVM
+{
+    internal class ViewModelBase : DependencyObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
